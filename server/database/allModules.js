5534f6e98a3992025680f1c3dd2deb5ea7932beb
Umang_Varotariya <umangvarotariya007@gmail.com>
2022-09-22 18:35:34 +0530

    created

---
+++ server/database/allModules.js
@@ -0,0 +1,17 @@
+import { FoodModal } from './food'
+import { ImageModal } from './image'
+import { OrderModal } from './order'
+import { RestaurentModal } from './restaurant'
+import { MenuModal } from './menu'
+import { ReviewModal } from './review'
+import { UserModal } from './user'
+
+export {
+    FoodModal,
+    OrderModal,
+    RestaurentModal,
+    ImageModal,
+    MenuModal,
+    ReviewModal,
+    UserModal,
+};
