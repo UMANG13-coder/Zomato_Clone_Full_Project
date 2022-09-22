6bc94ac817fe182c91168a1810997906c60925ad
Umang_Varotariya <umangvarotariya007@gmail.com>
2022-09-22 18:22:51 +0530

    Created

---
+++ server/index.js
@@ -0,0 +1,27 @@
+import express from 'express'
+import dotenv from 'dotenv'
+
+import ConnectDB from './database/connection'
+
+dotenv.config();
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.status(200).json({
+        message: "server is run"
+    })
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+    // ConnectDB().then(() => {
+    //     console.log('Server is Running !');
+    // }).catch((err) => {
+    //     console.log('DataBase connection Failed !!');
+    //     console.log(err);
+    // });
+});
