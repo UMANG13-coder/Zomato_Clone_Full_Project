5c9d9c74eefe5a32402934b4311c190db3d3a266
Umang_Varotariya <umangvarotariya007@gmail.com>
2022-09-27 08:58:33 +0530

    created

---
+++ server/api/food/index.js
@@ -0,0 +1,71 @@
+import express from 'express'
+
+import { FoodModel } from '../../database/allModules'
+
+const Router = express.Router();
+
+/**
+ * Route    /:_id
+ * des      get food by _id
+ * params   _id
+ * access   public  
+ * Method   Get
+ * 
+ */
+Router.get('/:_id', async (req, res) => {
+    try {
+        const { _id } = req.params;
+        const foods = await FoodModel.findById(_id);
+        if (!foods) {
+            return res.status(400).json({ Error: 'Food item not found' });
+        }
+        return res.status(200).json({ foods });
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
+    }
+});
+
+/**
+ * Route     /r/:_id
+ * Des       Get all food based on particular restaurant
+ * Params    _id
+ * Access    Public
+ * Method    GET
+ */
+Router.get('/r/:_id', async (req, res) => {
+    try {
+        const { _id } = req.params;
+        const foods = await FoodModel.find({
+            restaurant: _id
+        });
+        if (!foods) {
+            return res.status(400).json({ Error: 'Food item not found' });
+        }
+        return res.status(200).json({ foods });
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
+    }
+});
+
+/**
+ * Route     /c/:category
+ * Des       Get all foods based on category 
+ * Params    category
+ * Access    Public
+ * Method    GET
+ */
+Router.get('/c/:category', async (req, res) => {
+    try {
+        const { category } = req.params;
+        const foods = await FoodModel.find({
+            catagory: { $regex: category, $options: 'i' }
+        });
+        if (!foods) {
+            return res.status(400).json({ Error: 'Food item not found' });
+        }
+
+        return res.status(200).json({ foods });
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
+    }
+});
