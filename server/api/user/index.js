fabadc43357ba7fc94545efca8cb57fb777a4008
Umang_Varotariya <umangvarotariya007@gmail.com>
2022-10-01 17:45:57 +0530

    Updated

--- server/api/user/index.js
+++ server/api/user/index.js
@@ -1,5 +1,5 @@
 import express from "express";
-// import { UserModel } from "../../database/allModels";
+import { UserModel } from "../../database/allModules";
 import passport from "passport";
 
 const Router = express.Router();
@@ -24,5 +24,25 @@ Router.get(
     }
   }
 );
+
+/**
+ * Route     /:_id
+ * Des       Get user data (For review system) by _id
+ * Params    _id
+ * Access    Public
+ * Method    GET
+ */
+Router.get('/:_id', async (req, res) => {
+  try {
+    const { _id } = req.params;
+    const getUser = await UserModel.findById(_id);
+    const { fullName } = getUser;
+    if (!getUser) return res.status(404).json({ message: 'User not Found !' });
+    return res.status(200).json({ fullName });
+
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+})
 
 export default Router;
